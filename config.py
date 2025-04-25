926a8f53e20b722268f662fde08ebf5a39ca7ddf
vincent1go <isaievdmitro@icloud.com>
2025-04-25 10:53:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Токен бота и другие параметры из переменных окружения
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7511704960:AAH-rIBOQkhzYvTVOp2tiodxzK3vp8GUiYM")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7511704960:AAFKDWgg2-cAzRxywX1gXK47OQRWJi72qGw")
 ADMIN_ID = os.getenv("ADMIN_ID", "6587507343")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://telegram-pdf-bot-1f5c.onrender.com/telegram")
 
